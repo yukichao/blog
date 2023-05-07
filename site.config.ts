430711303a5cdde280df14ac48cdf0b24237ca99
赵宇琦 <1552645119@qq.com>
2023-05-07 19:19:09 +0800

    Chore: update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'fa0f4fb044c74f8cb2243ea16dd584e9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: "Yuki's Blog",
+  domain: 'www.zhaoyuqi.com.cn',
+  author: 'Yuki',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
